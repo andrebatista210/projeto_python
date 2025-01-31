9bfcbd144bedfe0b4c8f8a6b15fd383f12bbf124
Andre L Batsista <andrebatista210@gmail.com>
2025-01-31 19:50:13 -0300

    Exercicio 2

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello')
+
+# texto = input("Digite seu nome: ")
+# print(len(texto))
+
+i = 0
+
+for _ in range(2):
+    i = i + int(input("Digite seu número a ser somado: "))
+
+print(f'A soma dos numeros é {i}')
