710113910daffa7520855a4b148a4b5c20d2c17e
Andre L Batsista <andrebatista210@gmail.com>
2025-01-31 20:10:27 -0300

    Exercicio 3

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 
+# Exercicio 1
+
 # texto = input("Digite seu nome: ")
 # print(len(texto))
 
-i = 0
+# Exercicio 2
 
-for _ in range(2):
-    i = i + int(input("Digite seu número a ser somado: "))
+# i = 0
 
-print(f'A soma dos numeros é {i}')
+# for _ in range(2):
+#     i = i + int(input("Digite seu número a ser somado: "))
+
+# print(f'A soma dos numeros é {i}')
+
+# Exercicio 3
+
+nome = input("DIGITE SEU NOME: ")
+salario = float(input("DIGITE SEU SALARIO: "))
+percentual_bunus = float(input("DIGITE SEU PERCENTUAL BONUS: "))
+
+tot = ((salario * percentual_bunus)/ 100) + 1000 + salario
+
+print("Cáculo de bonus com Entrada do Usuario")
+print(f"Cáculo do KPI do bonus de 2024 é de {tot}")
