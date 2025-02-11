149bfca9d92337b2b8885ff567182e5b7bf005e0
Andre L Batsista <andrebatista210@gmail.com>
2025-02-11 20:05:02 -0300

    Adicionando ACTION

--- aula_01/main.py
+++ aula_01/main.py
@@ -15,13 +15,13 @@
 
 # Exercicio 3
 
-nome = input("DIGITE SEU NOME: ")
-salario = float(input("DIGITE SEU SALARIO: "))
-percentual_bunus = float(input("DIGITE SEU PERCENTUAL BONUS: "))
+# nome = input("DIGITE SEU NOME: ")
+# salario = float(input("DIGITE SEU SALARIO: "))
+# percentual_bunus = float(input("DIGITE SEU PERCENTUAL BONUS: "))
 
-tot = ((salario * percentual_bunus)/ 100) + 1000 + salario
+# tot = ((salario * percentual_bunus)/ 100) + 1000 + salario
 
-print("Cáculo de bonus com Entrada do Usuario")
-print(f"Cáculo do KPI do bonus de 2024 é de {tot}")
+# print("Cáculo de bonus com Entrada do Usuario")
+# print(f"Cáculo do KPI do bonus de 2024 é de {tot}")
 
-UVA@25051978
+print('TESTE DE ACTION!')
